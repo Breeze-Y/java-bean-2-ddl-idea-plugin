26d639af53494207fdde912f31b682f7cbf900ef
yuchengxin <yuchengxin@xiaomalixing.com>
2021-01-29 19:43:20 +0800

    feature: project init

---
+++ src/main/java/com/breezes/javabean2ddl/main/MainAction.java
@@ -0,0 +1,17 @@
+package com.breezes.javabean2ddl.main;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author yuchengxin@xiaomalixing.com
+ * @date 2021/1/29 18:56
+ * @description 插件主入口
+ */
+public class MainAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
